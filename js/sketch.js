2b91d078750d147ef264d4b92227d9d91e9d75cb
joelgoughbecker <116875565+joelgoughbecker@users.noreply.github.com>
2022-11-15 12:08:43 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,43 @@
+var widthRect = 50;
+var heightRect = 50;
+
+
 function setup() {
     var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
+    canvas.parent("p5container"); 
+    background(220);
 }
 
 function draw() {
-    background(220);
+   
 
+   
+}
+function mousePressed(){
+
+if(mouseButton ==LEFT){ 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+ rectMode(CENTER);
+    rect(mouseX, mouseY, widthRect,heightRect);
+}
+if(mouseButton ==RIGHT){
+    fill(255, 255, 255);
+ rectMode(CENTER);
+    ellipse(mouseX, mouseY, widthRect,heightRect);
+}
+}
+
+function keyPressed(){
+ if(keyCode == ENTER){ 
+    background(0);
+ }
+
+ if(keyCode == UP_ARROW){
+     fill(255)
+     triangle(mouseX, mouseY, mouseX+50, mouseY, mouseX+25, mouseY-50);
+    }
+ if(keyCode == DOWN_ARROW){
+     fill(random(0,255), random(0,255), random(0,255));
+     rect(mouseX, mouseY, widthRect+20, heightRect+10)}
+    
 }
